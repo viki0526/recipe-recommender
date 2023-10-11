77a0f670668f97642ef6410304c8787e71cd8597
viki0526 <77221209+viki0526@users.noreply.github.com>
2023-10-11 23:05:10 +0800

    clean up dataset, make migration more efficient

--- backend/migrate.py
+++ backend/migrate.py
@@ -1,31 +1,42 @@
 ### Migrate csv dataset to Postgres database
+import logging
 import pandas as pd
 import ast
 from app import Recipe, Ingredient, RecipeIngredient, db, app
+from sqlalchemy import create_engine
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+
 
 def main():
     df = pd.read_csv('RecipesDataset/recipes_data.csv')
     df['NER'] = df['NER'].apply(lambda x: set(ast.literal_eval(x)))
+    df = df[df['NER'] != set()]
 
-    # Populate ingredients table with all ingredients in dataset
     all_ingredients = set.union(*df['NER'].tolist())
-    for ingredient in all_ingredients:
-        ing = Ingredient(ingredient)
-        db.session.add(ing)
-    db.session.commit()
+    ingredients_df = pd.DataFrame(list(all_ingredients), columns=['name'])
 
-    # Populate recipes table and recipes-ingredients relation table
-    for idx, row in df.iterrows():
-        name = row['title']
-        link = row['link']
-        site = row['site']
-        recipe = Recipe(idx, name, link, site)
-        db.session.add(recipe)
-        for ingredient in row['NER']:
-            recipe_ingredient = RecipeIngredient(idx, ingredient)
-            db.session.add(recipe_ingredient)
-    db.session.commit()
+    recipes_df = df.drop(columns=['ingredients', 'directions', 'source', 'NER']).rename(columns={'title': 'name'})
+    recipes_df['id'] = df.index
+    recipes_df = recipes_df[['id', 'name', 'link', 'site']]
+
+    recipe_ingredients_df = df.explode('NER')
+    recipe_ingredients_df['recipe_id'] = recipe_ingredients_df.index
+    recipe_ingredients_df = recipe_ingredients_df.drop(columns=['title', 'ingredients', 'directions', 'link', 'source', 'site']).rename(columns={'NER': 'ingredient'}).reset_index()
+    recipe_ingredients_df = recipe_ingredients_df[['recipe_id', 'ingredient']]
+
+    logging.debug('Created dfs')
+
+    ingredients_df.to_sql('ingredients', engine, if_exists='append', index=False)
+    logging.debug('Created ingredients table')
+
+    recipes_df.to_sql('recipes', engine, if_exists='append', index=False)
+    logging.debug('Created recipes table')
+
+    recipe_ingredients_df.to_sql('recipe_ingredients', engine, if_exists='append', index=False)
+    logging.debug('Created recipe_ingredients table')
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='migrate.log', level=logging.DEBUG)
     with app.app_context():
         main()
