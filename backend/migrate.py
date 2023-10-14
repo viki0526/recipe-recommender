df56ee366d33b67857243042cdfaea49d0df4544
viki0526 <77221209+viki0526@users.noreply.github.com>
2023-10-15 00:41:42 +0800

    modify migration script to match new pipeline

--- backend/migrate.py
+++ backend/migrate.py
@@ -1,11 +1,41 @@
-### Migrate csv dataset to Postgres database
+### Clean up, validate and migrate csv dataset to Postgres database
 import logging
 import pandas as pd
 import ast
+import re
+import inflect
+import json
 from app import Recipe, Ingredient, RecipeIngredient, db, app
 from sqlalchemy import create_engine
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+
+def remove_non_alphabet(s):
+    return {re.sub(r'[^a-zA-Z ]', '', word) for word in s}
+
+def check_ingredients(s, ings):
+    food_items = s.intersection(ings)
+    return food_items
+
+def get_ingredients():
+    p = inflect.engine()
+    with open('RecipesDataset/train.json', 'r') as file:
+        data = json.load(file)
+    ings = set()
+    for recipe in data:
+        s = recipe["ingredients"]
+        ings.update(s)
+    opposite = set()
+    for x in ings:
+        alt = None
+        if p.singular_noun(x):
+            alt = p.singular_noun(x)
+        else:
+            alt = p.plural(x)
+        if alt:
+            opposite.add(alt)
+    ings = ings.union(opposite)
+    return ings
 
 def main():
     # Create schema
@@ -15,9 +45,15 @@ def main():
     df = pd.read_csv('RecipesDataset/recipes_data.csv')
     df['NER'] = df['NER'].apply(lambda x: set(ast.literal_eval(x)))
     df = df[df['NER'] != set()]
+    df['NER'] = df['NER'].apply(remove_non_alphabet)
+
+    # Create validation set of over 1500 ingredients
+    all_ingredients = get_ingredients()
+
+    # Clean up NER column by matching to ingredient validation set
+    df['NER'] = df['NER'].apply(lambda x: check_ingredients(x, all_ingredients))
 
     # Create dataframes for each table
-    all_ingredients = set.union(*df['NER'].tolist())
     ingredients_df = pd.DataFrame(list(all_ingredients), columns=['name'])
 
     recipes_df = df.drop(columns=['ingredients', 'directions', 'source', 'NER']).rename(columns={'title': 'name'})
@@ -31,15 +67,15 @@ def main():
 
     logging.debug('Created dfs')
 
-    # Create tables
+    # Populate tables
     ingredients_df.to_sql('ingredients', engine, if_exists='append', index=False)
-    logging.debug('Created ingredients table')
+    logging.debug('Populated ingredients table')
 
     recipes_df.to_sql('recipes', engine, if_exists='append', index=False)
-    logging.debug('Created recipes table')
+    logging.debug('Populated recipes table')
 
     recipe_ingredients_df.to_sql('recipe_ingredients', engine, if_exists='append', index=False)
-    logging.debug('Created recipe_ingredients table')
+    logging.debug('Populated recipe_ingredients table')
 
 if __name__ == '__main__':
     logging.basicConfig(filename='app.log', level=logging.DEBUG)
