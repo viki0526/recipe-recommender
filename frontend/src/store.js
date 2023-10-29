2f5c5c0756c884ebe76654f65f9e16c5678e1c22
viki0526 <77221209+viki0526@users.noreply.github.com>
2023-10-29 13:42:33 +0800

    add persistent redux store with localStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Use 'localStorage' as the storage engine
 import ingredientFiltersReducer from './reducers/ingredientFilters';
 
-const store = configureStore({
-    reducer: {
-        ingredientFilters: ingredientFiltersReducer,
-        // Add more reducers as needed
-    },
+const rootReducer = combineReducers({
+    ingredientFilters: ingredientFiltersReducer,
+    // Add more reducers as needed
 });
+
+const persistConfig = {
+    key: 'root',
+    storage, // Use 'localStorage' as the storage engine
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer
+    // Additional store configuration options go here
+});
+
+export const persistor = persistStore(store); // Export the persistor
 
 export default store;
